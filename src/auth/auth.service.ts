cb864bf7e985cbda424ae541a0918e4c8c417f56
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2024-01-17 17:31:23 -0600

    feat: login test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -37,10 +37,15 @@ export class AuthService {
     console.log({ environment: this.environment });
     return {
       httpOnly: true,
+      sameSite: 'none',
+      //httpOnly: true,
       //secure: this.environment !== 'development',
       //secure: false,
-      sameSite: 'strict',
-      path: '/',
+      //sameSite: 'strict',
+      //path: '/',
+
+      //secure: ENVIRONMENT === "production",
+      //sameSite: ENVIRONMENT === "production" ? "none" : "lax",
     };
   }
 
