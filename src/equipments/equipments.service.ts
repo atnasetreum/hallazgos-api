97767157c700ab3705d6498ab2dec6f9b630cb99
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-03-04 14:44:57 -0600

    feat: backup

--- src/equipments/equipments.service.ts
+++ src/equipments/equipments.service.ts
@@ -16,98 +16,107 @@ export class EquipmentsService {
   ) {}
 
   create(createEquipmentDto: CreateEquipmentDto) {
+    this.equipmentCostHistoryRepository;
     return createEquipmentDto;
   }
 
   async seed() {
-    await this.equipmentRepository.update(
+    const data = [
+      { name: 'ADAPTADOR FILTRO 750036', deliveryFrequency: 90 },
+      { name: 'ARAÑA PARA CASCO (SIN IVA)', deliveryFrequency: 90 },
+      { name: 'BARBUQUEJO DE 3 PUNTOS PARA CASCO 3M', deliveryFrequency: 365 },
+      { name: 'BOTAS WESLANTD 9309 ', deliveryFrequency: 365 },
+      { name: 'BOTA BLANCA PU-BIODENSIDAD - SI', deliveryFrequency: 365 },
+      { name: 'BOTA NEGRA PU-BIODENSIDAD - SI', deliveryFrequency: 365 },
       {
-        isActive: true,
+        name: 'BOTA SEG/ING STEEL SERIE REVOLUTION   SI',
+        deliveryFrequency: 365,
       },
+      { name: 'BOTAS PARA SOLDADOR', deliveryFrequency: 365 },
+      { name: 'CARETA PARA SOLDAR', deliveryFrequency: 365 },
       {
-        manufacturingPlant: {
-          id: 2,
-        },
+        name: 'CARETA PROTECTOR FACIAL VISOR AMARILLA',
+        deliveryFrequency: 365,
       },
-    );
-
-    const manufacturingPlantId = 3;
-
-    const equipments = [
-      { name: 'ADAPTADOR FILTRO 750036', price: '0' },
-      { name: 'ARAÑA PARA CASCO (SIN IVA)', price: '0' },
-      { name: 'BARBUQUEJO DE 3 PUNTOS PARA CASCO 3M', price: '0' },
-      { name: 'BOTAS WESLANTD 9309 ', price: '0' },
-      { name: 'BOTA BLANCA PU-BIODENSIDAD - SI', price: '0' },
-      { name: 'BOTA NEGRA PU-BIODENSIDAD - SI', price: '0' },
-      { name: 'BOTA SEG/ING STEEL SERIE REVOLUTION   SI', price: '0' },
-      { name: 'BOTAS PARA SOLDADOR', price: '0' },
-      { name: 'CARETA PARA SOLDAR', price: '0' },
-      { name: 'CARETA PROTECTOR FACIAL VISOR AMARILLA', price: '0' },
-      { name: 'CASCO SEGURIDAD  3M SI', price: '0' },
-      { name: 'CHALECO REFLECTIVO ( SIN IVA )', price: '0' },
-      { name: 'CONJUNTO IMPERMEABLE EN PVC ( SIN IVA )', price: '0' },
-      { name: 'DELANTAL  DE CARNAZA (CON IVA)', price: '0' },
-      { name: 'DELANTAL BLANCO EN PVC (CON IVA)', price: '0' },
-      { name: 'FILTRO 3M PARTICULAS N95   SI', price: '0' },
-      { name: 'FILTRO PARA HUMOS METALICOS HONEYWELL', price: '0' },
-      { name: 'FILTROS PARA SOLDADURA 3M No 2097', price: '0' },
-      { name: 'GAFAS DE SEGURIDAD OSCURAS CAJA X 12', price: '0' },
-      { name: 'GAFAS DE SEGURIDAD TRANSPARENTE   SI', price: '0' },
-      { name: 'GORRA TIPO MONJA  (SST)', price: '0' },
-      { name: 'GUANTE ANSELL HYFLEX 11-561', price: '0' },
-      { name: 'GUANTE ANTI CORTE NIVEL 5 (SIN IVA)', price: '0' },
-      { name: 'GUANTE DE NITRILO LARGO(CI)', price: '0' },
-      { name: 'GUANTE HYFLEX 11-840 ANSELL(CI)', price: '0' },
-      { name: 'GUANTE HYFLEX 11-939 ANSELL', price: '0' },
-      { name: 'GUANTE LARGO PVC ALPHATEC 23-201', price: '0' },
-      { name: 'GUANTE TIPO VAQUETA CORTO (SIN IVA)', price: '0' },
-      { name: 'GUANTE VAQUETA SENCILLO TIPO LARGO', price: '0' },
-      { name: 'GUANTES DE NITRILO VERDE LARGOS', price: '0' },
-      { name: 'GUANTES PROTECCIÓN ARCO ELECTRICO 80-813', price: '0' },
-      { name: 'GUANTES TIPO INGENIERO LARGOS 12"', price: '0' },
-      { name: 'MONOGAFAS', price: '0' },
-      { name: 'PREFILTRO 3M REF.: 5N11', price: '0' },
-      { name: 'PREFILTRO PARTICULAS 7506N95', price: '0' },
-      { name: 'PROTECTOR AUDITIVO DE COPA PARA CASCO SI', price: '0' },
-      { name: 'PROTECTOR AUDITIVO TIPO COPA  SI', price: '0' },
-      { name: 'PROTECTOR FACIAL PARA CASCO SEGURIDAD 3M', price: '0' },
-      { name: 'RESPIRADOR 3M 9010', price: '0' },
-      { name: 'RESPIRADOR MEDIA CARA DE SILICONA 3M', price: '0' },
-      { name: 'RESPIRADOR SILICONA 7700/30M', price: '0' },
-      { name: 'RODILLERA PAR REF 2016904 MAVERICK', price: '0' },
-      { name: 'TRAJE TYCHEM 2000', price: '0' },
-      { name: 'TRAJE TYCHEM 400', price: '0' },
-      { name: 'TRAJE TYVECK PROTECCIÓN A QUIMICO L', price: '0' },
-      { name: 'TRAJE TYVECK TALLA L  PARA INACTIVAR', price: '0' },
-      { name: 'TRAJE TYVECK TALLA L (SIN IVA)', price: '0' },
-      { name: 'GUANTE RESISTENTE AL CALOR 43-113 KEVLAR', price: '0' },
-      { name: 'PROTECTOR SOLAR FACIAL (SIN IVA)', price: '0' },
-      { name: 'SOBRELENTES DE SEGURIDAD', price: '0' },
-      { name: 'PROTECTOR AUDITIVO DE INSERCCION(SI)', price: '0' },
-      { name: 'CARTUCHO VAPORES ORGANICOS 7500/1', price: '0' },
-      { name: 'CARTUCHO FILTROS PARA QUIMICOS 6001  SI', price: '0' },
-      { name: 'MONJA DE ALGODON PARA SOLDAR (SIN IVA)', price: '0' },
-      { name: 'CAPUCHA  DE SOL  CI', price: '0' },
-      { name: 'CARTUCHO  FILTRO PARA QUIMICO  3M 7093', price: '0' },
-      { name: 'RETENEDOR PARA FILTRO 3M  REF- 501 SI', price: '0' },
-      { name: 'GUANTES ALPHATEC-16-650', price: '0' },
-      { name: 'GUANTE ANSELL MICROFLEX  93-260', price: '0' },
+      { name: 'CASCO SEGURIDAD  3M SI', deliveryFrequency: 365 },
+      { name: 'CHALECO REFLECTIVO ( SIN IVA )', deliveryFrequency: 90 },
+      {
+        name: 'CONJUNTO IMPERMEABLE EN PVC ( SIN IVA )',
+        deliveryFrequency: 90,
+      },
+      { name: 'DELANTAL  DE CARNAZA (CON IVA)', deliveryFrequency: 90 },
+      { name: 'DELANTAL BLANCO EN PVC (CON IVA)', deliveryFrequency: 45 },
+      { name: 'FILTRO 3M PARTICULAS N95   SI', deliveryFrequency: 60 },
+      { name: 'FILTRO PARA HUMOS METALICOS HONEYWELL', deliveryFrequency: 30 },
+      { name: 'FILTROS PARA SOLDADURA 3M No 2097', deliveryFrequency: 30 },
+      { name: 'GAFAS DE SEGURIDAD OSCURAS CAJA X 12', deliveryFrequency: 60 },
+      { name: 'GAFAS DE SEGURIDAD TRANSPARENTE   SI', deliveryFrequency: 45 },
+      { name: 'GORRA TIPO MONJA  (SST)', deliveryFrequency: 90 },
+      { name: 'GUANTE ANSELL HYFLEX 11-561', deliveryFrequency: 15 },
+      { name: 'GUANTE ANTI CORTE NIVEL 5 (SIN IVA)', deliveryFrequency: 30 },
+      { name: 'GUANTE DE NITRILO LARGO(CI)', deliveryFrequency: 30 },
+      { name: 'GUANTE HYFLEX 11-840 ANSELL(CI)', deliveryFrequency: 15 },
+      { name: 'GUANTE HYFLEX 11-939 ANSELL', deliveryFrequency: 15 },
+      { name: 'GUANTE LARGO PVC ALPHATEC 23-201', deliveryFrequency: 60 },
+      { name: 'GUANTE TIPO VAQUETA CORTO (SIN IVA)', deliveryFrequency: 30 },
+      { name: 'GUANTE VAQUETA SENCILLO TIPO LARGO', deliveryFrequency: 30 },
+      { name: 'GUANTES DE NITRILO VERDE LARGOS', deliveryFrequency: 30 },
+      {
+        name: 'GUANTES PROTECCIÓN ARCO ELECTRICO 80-813',
+        deliveryFrequency: 180,
+      },
+      { name: `GUANTES TIPO INGENIERO LARGOS 12"`, deliveryFrequency: 30 },
+      { name: 'MONOGAFAS', deliveryFrequency: 90 },
+      { name: 'PREFILTRO 3M REF.: 5N11', deliveryFrequency: 30 },
+      { name: 'PREFILTRO PARTICULAS 7506N95', deliveryFrequency: 30 },
+      {
+        name: 'PROTECTOR AUDITIVO DE COPA PARA CASCO SI',
+        deliveryFrequency: 365,
+      },
+      { name: 'PROTECTOR AUDITIVO TIPO COPA  SI', deliveryFrequency: 365 },
+      {
+        name: 'PROTECTOR FACIAL PARA CASCO SEGURIDAD 3M',
+        deliveryFrequency: 365,
+      },
+      { name: 'RESPIRADOR 3M 9010', deliveryFrequency: 8 },
+      { name: 'RESPIRADOR MEDIA CARA DE SILICONA 3M', deliveryFrequency: 180 },
+      { name: 'RESPIRADOR SILICONA 7700/30M', deliveryFrequency: 365 },
+      { name: 'RODILLERA PAR REF 2016904 MAVERICK', deliveryFrequency: 90 },
+      { name: 'TRAJE TYCHEM 2000', deliveryFrequency: 8 },
+      { name: 'TRAJE TYCHEM 400', deliveryFrequency: 30 },
+      { name: 'TRAJE TYVECK PROTECCIÓN A QUIMICO L', deliveryFrequency: 30 },
+      { name: 'TRAJE TYVECK TALLA L  PARA INACTIVAR', deliveryFrequency: 30 },
+      { name: 'TRAJE TYVECK TALLA L (SIN IVA)', deliveryFrequency: 30 },
+      {
+        name: 'GUANTE RESISTENTE AL CALOR 43-113 KEVLAR',
+        deliveryFrequency: 45,
+      },
+      { name: 'PROTECTOR SOLAR FACIAL (SIN IVA)', deliveryFrequency: 90 },
+      { name: 'SOBRELENTES DE SEGURIDAD', deliveryFrequency: 90 },
+      { name: 'PROTECTOR AUDITIVO DE INSERCCION(SI)', deliveryFrequency: 120 },
+      { name: 'CARTUCHO VAPORES ORGANICOS 7500/1', deliveryFrequency: 60 },
+      {
+        name: 'CARTUCHO FILTROS PARA QUIMICOS 6001  SI',
+        deliveryFrequency: 60,
+      },
+      { name: 'MONJA DE ALGODON PARA SOLDAR (SIN IVA)', deliveryFrequency: 90 },
+      { name: 'CAPUCHA  DE SOL  CI', deliveryFrequency: 90 },
+      { name: 'CARTUCHO  FILTRO PARA QUIMICO  3M 7093', deliveryFrequency: 60 },
+      { name: 'RETENEDOR PARA FILTRO 3M  REF- 501 SI', deliveryFrequency: 90 },
+      { name: 'GUANTES ALPHATEC-16-650', deliveryFrequency: 60 },
+      { name: 'GUANTE ANSELL MICROFLEX  93-260', deliveryFrequency: 8 },
     ];
 
-    for (const equipment of equipments) {
-      const equipmentNew = await this.equipmentRepository.save({
-        name: equipment.name,
-        manufacturingPlant: {
-          id: manufacturingPlantId,
-        },
+    for (const { name, deliveryFrequency } of data) {
+      const equipment = await this.equipmentRepository.findOne({
+        where: { name },
       });
 
-      await this.equipmentCostHistoryRepository.save({
-        price: Number(equipment.price),
-        equipment: equipmentNew,
-        captureDate: new Date(),
-      });
+      if (equipment) {
+        await this.equipmentRepository.update(equipment.id, {
+          deliveryFrequency,
+        });
+      }
     }
 
     return {
