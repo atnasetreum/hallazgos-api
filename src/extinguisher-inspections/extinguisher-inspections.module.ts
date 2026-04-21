9b1af62ca0af712f087e4ff31b5979aa8959e71e
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-04-20 18:06:34 -0600

    feat: backup

--- src/extinguisher-inspections/extinguisher-inspections.module.ts
+++ src/extinguisher-inspections/extinguisher-inspections.module.ts
@@ -1,10 +1,8 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
-import { EmergencyTeam } from 'emergency-teams/entities/emergency-team.entity';
 import { ExtinguisherInspectionEvaluation } from './entities/extinguisher-inspection-evaluation.entity';
 import { ExtinguisherInspectionsController } from './extinguisher-inspections.controller';
-import { ExtinguisherInspectionsSeedService } from './extinguisher-inspections-seed.service';
 import { ExtinguisherInspectionsService } from './extinguisher-inspections.service';
 import { ExtinguisherInspection } from './entities/extinguisher-inspection.entity';
 
@@ -13,14 +11,10 @@ import { ExtinguisherInspection } from './entities/extinguisher-inspection.entit
     TypeOrmModule.forFeature([
       ExtinguisherInspection,
       ExtinguisherInspectionEvaluation,
-      EmergencyTeam,
     ]),
   ],
   controllers: [ExtinguisherInspectionsController],
-  providers: [
-    ExtinguisherInspectionsService,
-    ExtinguisherInspectionsSeedService,
-  ],
+  providers: [ExtinguisherInspectionsService],
   exports: [TypeOrmModule, ExtinguisherInspectionsService],
 })
 export class ExtinguisherInspectionsModule {}
